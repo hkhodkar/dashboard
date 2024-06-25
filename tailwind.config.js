32d91c8351be3b2cee7029966567ca81def441da
hatef khodkar <hatef.khodkar@hotmail.com>
2024-06-25 20:04:02 +0400

    feat(ui-kit): add ui-kit package to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import { themeColors } from "@hatef_khodkar/storybook";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: themeColors,
   },
   plugins: [],
 };
