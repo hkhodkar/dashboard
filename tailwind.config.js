eda6dcc3f715f9e04d0d03a0ddbc277fdc82c01a
hatef khodkar <hatef.khodkar@hotmail.com>
2024-06-27 16:52:50 +0400

    chore(font): add roboto font to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ import { themeColors } from "@hatef_khodkar/storybook";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-mode="dark"]'],
   theme: {
-    extend: {},
-    colors: themeColors,
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+    colors: {
+      ...themeColors,
+      "main-background": "#d7e8f7",
+    },
   },
   plugins: [],
 };
